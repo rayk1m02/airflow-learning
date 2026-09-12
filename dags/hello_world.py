0d8a2d7281dba6062aa501a568bf59d1ea556059
Raymond Kim <hkapray@gmail.com>
2026-09-12 12:29:54 -0500

    add second tasks

--- dags/hello_world.py
+++ dags/hello_world.py
@@ -1,10 +1,14 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
 from datetime import datetime
+
+def print_message():
+    print("This task ran via PythonOperator")
 
 with DAG(
     dag_id="hello_world",
-    start_date=datetime(2026, 9, 12),
+    start_date=datetime(2026, 9, 1),
     schedule=None,
     catchup=False,
 ) as dag:
@@ -13,3 +17,10 @@ with DAG(
         task_id="say_hello",
         bash_command="echo 'Hello from Airflow'"
     )
+
+    say_python = PythonOperator(
+        task_id="say_python",
+        python_callable=print_message
+    )
+
+    say_hello >> say_python # dependency syntax (run say_hello before say_python
