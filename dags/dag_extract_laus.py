b457f20c5bd4aecd6dde4b1b67be1133c95fe533
Raymond Kim <hkapray@gmail.com>
2026-09-12 21:57:49 -0500

    dag for extract_laus

---
+++ dags/dag_extract_laus.py
@@ -0,0 +1,15 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+with DAG(
+    dag_id="de_market_analysis_pipeline",
+    start_date=datetime(2026, 9, 1),
+    schedule=None,
+    catchup=False,
+) as dag:
+
+    extract_laus = BashOperator(
+        task_id="extract_laus"
+        bash_command="cd /opt/de-market-analysis && python extract/extract_laus.py"
+    )
